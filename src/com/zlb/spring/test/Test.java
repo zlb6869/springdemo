82a4877dae9e0ef9c5856446a5a45810071cefab
[zlb6869] <passw06d@123>
2022-05-03 19:07:50 +0800

    spring demo

--- src/com/zlb/spring/test/Test.java
+++ src/com/zlb/spring/test/Test.java
@@ -12,8 +12,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.lang.reflect.Proxy;
 
 public class Test {
+
     @org.junit.Test
     public void show() throws Exception{
+        System.out.println("hhhhhhhhhhhhhhhhh");
+        System.out.println("hhhhhhhhhhhhhhhhh");
+        System.out.println("hhhhhhhhhhhhhhhhh");
         MyInvocationHandler myInvocationHandler = new MyInvocationHandler( new Haimpl());
         Ha o = (Ha) Proxy.newProxyInstance(Haimpl.class.getClassLoader(), Haimpl.class.getInterfaces(), myInvocationHandler);
         o.show(new Haha());
